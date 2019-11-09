93c230c0cae7d09d1e4f260865e71c77e554a50d
markurp <markus.pettrem@hotmail.com>
2019-11-09 11:12:28 +0100

    Endret mediascreen på store bilder

--- gjomo_tannklinikk/js/about.js
+++ gjomo_tannklinikk/js/about.js
@@ -1,21 +1,5 @@
-function showMoreG(){
-  var text = document.getElementById("moreG");
-  var btnText = document.getElementById("myButton");
-
-  console.log("HEII")
-
-  if (text.style.display === "inline"){
-    text.style.display = "none";
-    btnText.innerHTML = "Les mer";
-  }
-  else{
-    text.style.display = "inline";
-    btnText.innerHTML = "Les mindre";
-  }
-}
-
-function showMoreJ(){
-  var text = document.getElementById("moreJ");
+function showMore(){
+  var text = document.getElementById("more");
   var btnText = document.getElementById("myButton");
 
   console.log("HEII")
